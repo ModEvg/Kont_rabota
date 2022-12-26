652417b425b8bb13b68feec3b5e5d137530d1c17
ModinaEN <modinaen@gmail.com>
2022-12-26 07:51:54 +0300

    Задаем элементы массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. */
+
+Console.Write($"Введите количество элементов");
+int kol = int.Parse(Console.ReadLine()!);
+
+string[] massiv = new string[kol];
+
+for (int i = 0; i<massiv.Length; i++)
+    {   
+        Console.Write($"{i+1} 'элемент' ");
+        massiv[i] = Console.ReadLine()!; // Задаем элементы массива
+    }
+PrintArray(massiv);
+
+void PrintArray(string[] matr)    // Вывод массива
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+            Console.Write("{0,3}", matr[i, j]);
+    }
+}
